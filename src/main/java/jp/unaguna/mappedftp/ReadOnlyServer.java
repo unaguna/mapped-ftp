37019afe1cf0beb9db5656d5c412133177d2216e
unaguna <k.izumi.ysk@gmail.com>
2022-09-28 21:28:01 +0900

    🎉first commit

---
+++ src/main/java/jp/unaguna/mappedftp/ReadOnlyServer.java
@@ -0,0 +1,24 @@
+package jp.unaguna.mappedftp;
+
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.User;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.usermanager.UserFactory;
+
+public class ReadOnlyServer {
+    public static void main(String[] args) throws FtpException {
+        FtpServerFactory ftpServerFactory = new FtpServerFactory();
+
+        UserFactory userFactory = new UserFactory();
+        userFactory.setName("anonymous");
+        User anonymous = userFactory.createUser();
+
+        UserManager userManager = ftpServerFactory.getUserManager();
+        userManager.save(anonymous);
+
+        FtpServer server = ftpServerFactory.createServer();
+        server.start();
+    }
+}
