9b61dd4db78308ecc6e2dd39f80063129f965764
unaguna <k.izumi.ysk@gmail.com>
2022-10-23 00:01:17 +0900

    ✨allow to specify owner and group of files in xml config

--- src/test/java/jp/unaguna/mappedftp/config/spring/UrlFileBeanDefinitionParserTest.java
+++ src/test/java/jp/unaguna/mappedftp/config/spring/UrlFileBeanDefinitionParserTest.java
@@ -10,6 +10,8 @@ import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.ftpserver.impl.DefaultFtpServer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import java.net.URL;
@@ -31,6 +33,36 @@ public class UrlFileBeanDefinitionParserTest {
             final FileTreeNode fileTreeNode = (FileTreeNode) fileSystemView.getFile("/file1");
             final FileTreeItemFromURL file = (FileTreeItemFromURL) fileTreeNode.getFile();
             assertEquals("http://dummy1.example.com/", file.getSource().toString());
+            assertNull(file.getOwnerName());
+            assertNull(file.getGroupName());
+
+        } catch (FtpException e) {
+            fail(e);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "config__owner.xml, test_owner, ",
+            "config__group.xml, , test_group",
+            "config__owner_and_group.xml, test_owner, test_group",
+    })
+    public void testParse__with_owner_and_group(
+            String inputResourceName, String expectedOwnerName, String expectedGroupName, TestInfo testInfo
+    ) {
+        final URL configPath = TestUtils.getInputResource(inputResourceName, testInfo);
+
+        final FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext(configPath.toString());
+        final DefaultFtpServer actualServer = (DefaultFtpServer) ctx.getBean("testServer");
+        final MappingFileSystemFactory fileSystemFactory = (MappingFileSystemFactory) actualServer.getFileSystem();
+
+        try {
+            final LinkedFileSystemView fileSystemView = fileSystemFactory.createFileSystemView(new UserStub());
+            final FileTreeNode fileTreeNode = (FileTreeNode) fileSystemView.getFile("/file1");
+            final FileTreeItemFromURL file = (FileTreeItemFromURL) fileTreeNode.getFile();
+            assertEquals("http://dummy1.example.com/", file.getSource().toString());
+            assertEquals(expectedOwnerName, file.getOwnerName());
+            assertEquals(expectedGroupName, file.getGroupName());
 
         } catch (FtpException e) {
             fail(e);
