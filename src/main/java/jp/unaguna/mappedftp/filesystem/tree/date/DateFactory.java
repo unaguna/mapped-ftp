53c0716a60f6a8499cb6a8000e8b106cf2d1ce40
unaguna <k.izumi.ysk@gmail.com>
2022-10-23 14:40:57 +0900

    📝javadoc of DateFactory

--- src/main/java/jp/unaguna/mappedftp/filesystem/tree/date/DateFactory.java
+++ src/main/java/jp/unaguna/mappedftp/filesystem/tree/date/DateFactory.java
@@ -3,10 +3,21 @@ package jp.unaguna.mappedftp.filesystem.tree.date;
 public interface DateFactory {
     long getLong();
 
+    /**
+     * Returns a factory which create fixed time.
+     *
+     * @param value the fixed time
+     * @return a factory whose creation method returns the fixed time
+     */
     static DateFactory constance(long value) {
         return new ConstanceDateFactory(value);
     }
 
+    /**
+     * Returns a factory which creates timestamps each time.
+     *
+     * @return a factory whose creation method returns the time when the creation method is called
+     */
     static DateFactory eachTime() {
         return new CurrentDateFactory();
     }
